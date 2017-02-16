2fc9b201de30ccf09efffccce4235020c6049d59
alidabour <alidabour0@gmail.com>
2017-02-16 11:13:01 +0200

    Constraint Test

--- app/src/main/java/com/example/ali/homeschool/controller/activities/MainActivity.java
+++ app/src/main/java/com/example/ali/homeschool/controller/activities/MainActivity.java
@@ -1,22 +1,25 @@
 package com.example.ali.homeschool.controller.activities;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.ali.homeschool.R;
 
 
 public class MainActivity extends AppCompatActivity {
- WebView webView;
+TextView logoname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
-
+//        logoname = (TextView) findViewById(R.id.logoName);
+//        logoname.setTypeface(Typeface.createFromAsset(getAssets(),"Amiri-Bold.ttf"));
         //ImageView imageView = (ImageView) findViewById(R.id.imageView);
         Button Signup = (Button) findViewById(R.id.Signin);
 
