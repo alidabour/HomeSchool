a746fe3205878af3e8d8869bc0245e7af36c2747
manar_000 <manaar.mustapha@gmail.com>
2017-06-10 14:33:38 +0200

    yay

--- app/src/main/java/com/example/ali/homeschool/InstructorLessons/InstructorLessonsActivity.java
+++ app/src/main/java/com/example/ali/homeschool/InstructorLessons/InstructorLessonsActivity.java
@@ -103,7 +103,6 @@ public class InstructorLessonsActivity extends AppCompatActivity {
             courseID = courseCreated.getCourse_id().toString();
         }
 
-
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
