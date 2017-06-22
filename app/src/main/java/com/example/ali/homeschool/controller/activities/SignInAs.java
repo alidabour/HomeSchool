2c7135a69ba36f70516b63b97d9a065675ba74a9
alidabour <alidabour0@gmail.com>
2017-06-22 10:21:48 +0200

    disable button

--- app/src/main/java/com/example/ali/homeschool/controller/activities/SignInAs.java
+++ app/src/main/java/com/example/ali/homeschool/controller/activities/SignInAs.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.ali.homeschool.R;
@@ -34,45 +35,100 @@ public class SignInAs extends AppCompatActivity {
         Log.v("pager ", pager.toString());
         ArrayList<Home> res = new ArrayList<>();
         Home home = new Home() ;
-        home.setImage(R.drawable.instructor_icon);
-        home.setName(getString(R.string.Instructor));
-        res.add(home);
-        home = new Home();
         home.setImage(R.drawable.student_icon);
         home.setName(getString(R.string.Student));
+        res.add(home);
+        home = new Home();
+        home.setImage(R.drawable.instructor_icon);
+        home.setName(getString(R.string.Instructor));
         res.add(home);
         home = new Home();
         home.setImage(R.drawable.parents_icon);
         home.setName(getString(R.string.Parents));
         res.add(home);
         pager.setAdapter(new SignInAsAdapter(this, res));
-        ImageView next = (ImageView) findViewById(R.id.next);
+        final ImageButton previous = (ImageButton) findViewById(R.id.previous);
+        final ImageButton next = (ImageButton) findViewById(R.id.next);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                previous.setEnabled(true);
+//                previous.setSelected(true);
+//                int current =pager.getCurrentItem();
+//                if(current == pager.getChildCount()){
+//                    next.setEnabled(false);
+//                    next.setSelected(false);
+//                }else {
+//                    next.setEnabled(true);
+//                    next.setSelected(true);
+//
+//                }
                 pager.setCurrentItem(getItem(+1), true); //getItem(-1) for previous
 
             }
         });
-        ImageView previous = (ImageView) findViewById(R.id.previous);
+        previous.setEnabled(false);
+        previous.setVisibility(View.GONE);
         previous.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                next.setEnabled(true);
+//                next.setSelected(true);
+//                int current =pager.getCurrentItem();
+//                if(current == 0){
+//                    previous.setEnabled(false);
+//                    previous.setSelected(false);
+//                }else {
+//                    previous.setEnabled(true);
+//                    previous.setSelected(true);
+//                }
                 pager.setCurrentItem(getItem(-1), true); //getItem(-1) for previous
             }
         });
-        BitmapFactory.Options o = new BitmapFactory.Options();
-        o.inTargetDensity = DisplayMetrics.DENSITY_DEFAULT;
-        Bitmap bmp = BitmapFactory.decodeResource(getApplicationContext().getResources(),
-                R.drawable.arrow_2, o);
-        int w = bmp.getWidth();
-        int h = bmp.getHeight();
-        Bitmap bmp2 = BitmapFactory.decodeResource(getApplicationContext().getResources(),
-                R.drawable.arrow_1, o);
-        int w2 = bmp2.getWidth();
-        int h2 = bmp2.getHeight();
-        Glide.with(getApplicationContext()).load(R.drawable.arrow_2).override(w,h).into(next);
-        Glide.with(getApplicationContext()).load(R.drawable.arrow_1).override(w2,h2).into(previous);
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset,
+                                       int positionOffsetPixels) {
+//                Toast.makeText(SignInAs.this, position +"/"+positionOffset, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+//                Toast.makeText(SignInAs.this, "Count: "+pager.getChildCount(), Toast.LENGTH_SHORT).show();
+                if(position == 2){
+                    next.setEnabled(false);
+                    next.setVisibility(View.GONE);
+                }else{
+                    next.setEnabled(true);
+                    next.setVisibility(View.VISIBLE);
+
+                }
+                if(position==0){
+                    previous.setEnabled(false);
+                    previous.setVisibility(View.GONE);
+                }else{
+                    previous.setEnabled(true);
+                    previous.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+//        BitmapFactory.Options o = new BitmapFactory.Options();
+//        o.inTargetDensity = DisplayMetrics.DENSITY_DEFAULT;
+//        Bitmap bmp = BitmapFactory.decodeResource(getApplicationContext().getResources(),
+//                R.drawable.arrow_2, o);
+//        int w = bmp.getWidth();
+//        int h = bmp.getHeight();
+//        Bitmap bmp2 = BitmapFactory.decodeResource(getApplicationContext().getResources(),
+//                R.drawable.arrow_1, o);
+//        int w2 = bmp2.getWidth();
+//        int h2 = bmp2.getHeight();
+//        Glide.with(getApplicationContext()).load(R.drawable.arrow_2).override(w,h).into(next);
+//        Glide.with(getApplicationContext()).load(R.drawable.arrow_1).override(w2,h2).into(previous);
 
     }
 
