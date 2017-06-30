d27cd78aa722d5b6bff34e3ccd24361b8d7caf55
hossam <HossamEldeenOnsy@yahoo.com>
2017-06-30 04:39:57 +0200

    Parent

--- app/src/main/java/com/almanara/homeschool/parent/home/ParentActivityFragment.java
+++ app/src/main/java/com/almanara/homeschool/parent/home/ParentActivityFragment.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -36,8 +35,6 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.almanara.ali.homeschool.R.id.colors;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -138,8 +135,6 @@ import static com.almanara.ali.homeschool.R.id.colors;
                 builder.show();
 
 
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         });
         db = FirebaseDatabase.getInstance().getReference();
