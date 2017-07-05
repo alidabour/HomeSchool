a94d6238ec229d7bfa0c99eedac461d8461eda64
hossam <HossamEldeenOnsy@yahoo.com>
2017-07-05 12:37:43 +0200

    Hossam

--- app/src/main/java/com/almanara/homeschool/Almanara.java
+++ app/src/main/java/com/almanara/homeschool/Almanara.java
@@ -1,23 +1,23 @@
-package com.almanara.homeschool;
-
-import android.app.Application;
-
-import com.squareup.leakcanary.LeakCanary;
-
-/**
- * Created by Ali on 7/3/2017.
- */
-
-public class Almanara extends Application {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
-        // Normal app init code...
-    }
-}
+//package com.almanara.homeschool;
+//
+//import android.app.Application;
+//
+//import com.squareup.leakcanary.LeakCanary;
+//
+///**
+// * Created by Ali on 7/3/2017.
+// */
+//
+//public class Almanara extends Application {
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//        if (LeakCanary.isInAnalyzerProcess(this)) {
+//            // This process is dedicated to LeakCanary for heap analysis.
+//            // You should not init your app in this process.
+//            return;
+//        }
+//        LeakCanary.install(this);
+//        // Normal app init code...
+//    }
+//}
